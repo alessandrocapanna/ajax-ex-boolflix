b0c27be1fe7301fe57efa46bc61032d87800ccf1
alessandrocapanna <acapanna75@gmail.com>
2020-07-02 16:34:50 +0200

    milestone 3 √

--- js/script.js
+++ js/script.js
@@ -7,8 +7,12 @@ $(document).ready(
         $('ul#film-lista li').remove();
         // NOTE: salvo ricerca dell'utente in una variabile
         var ricercaUtente = $('input#ricerca-utente').val().toString();
+        if (ricercaUtente == '') {
+          alert('devi inserire il titolo del film o della serie')
+        }else {
+          searchMovies(ricercaUtente);
+        }
 
-        searchMovies(ricercaUtente);
       }
     );
 
@@ -46,23 +50,40 @@ $(document).ready(
       for (var i = 0; i < risultatiFilm.length; i++) {
         var singoloFilm = risultatiFilm[i];
 
+        if (singoloFilm.media_type !== 'person') {
+          var source = $("#film-template").html();
+          var template = Handlebars.compile(source);
 
-        var source = $("#film-template").html();
-        var template = Handlebars.compile(source);
+          var context = {
+            titolo: singoloFilm.title || singoloFilm.name,
+            titoloOriginale: singoloFilm.original_title || singoloFilm.original_name,
+            linguaOriginale:  lingua(singoloFilm.original_language),
+            votoUtente: stella(singoloFilm.vote_average),
+            filmSerie:filmSerie(singoloFilm.media_type),
+            urlPoster:poster(singoloFilm.poster_path)
+          };
+          var html = template(context);
 
-
-
-        var context = {
-          titolo: singoloFilm.title || singoloFilm.name,
-          titoloOriginale: singoloFilm.original_title || singoloFilm.original_name,
-          linguaOriginale:  lingua(singoloFilm.original_language),
-          votoUtente: stella(singoloFilm.vote_average),
-          filmSerie:singoloFilm.media_type
-        };
-        var html = template(context);
-
-        $('ul#film-lista').append(html);
+          $('ul#film-lista').append(html);
+        }
       }
+    }
+
+    // NOTE: funzione poster
+    function poster(url){
+      var img = '<img src="https://image.tmdb.org/t/p/w185' + url + '" alt="">';
+      return img;
+    }
+
+    // NOTE: funzione filmSerie
+    function filmSerie(tipo) {
+      var tipoDiContenuto = tipo;
+      if(tipoDiContenuto === 'movie'){
+        var tipoDiContenuto = 'film';
+      }else if(tipoDiContenuto === 'tv'){
+        var tipoDiContenuto = 'serie tv';
+      }
+      return tipoDiContenuto;
     }
 
     // NOTE: funzione immagine lingua o se non c'è lingua normale
